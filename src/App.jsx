de8b021fe16e8c02408d69576c8701991df2d314
mohamed.aziz.mhadhbi <mohamedaziz.mhadhbi@esprit.tn>
2024-02-02 23:01:57 +0100

    updating the input

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,34 @@
+import { useState } from "react"
+import "./style.css"
 
 function App() {
+  const [newItem, SetNewItem] = useState("")
   return (
     <>
-      <p> Hello </p>
+      <form className="new-item-form">
+        <div className="form-row">
+          <label htmlFor="item">New Item</label>
+          <input value={newItem} onChange={e => SetNewItem(e.target.value)} type="text" id="item" />
+        </div>
+        <button className="btn">Add</button>
+      </form>
+      <h1 className="header">Todo List</h1>
+      <ul className="list">
+        <li>
+          <label>
+            <input type="checkbox" />
+            Item 1
+          </label>
+          <button className="btn btn-danger">Delete</button>
+        </li>
+        <li>
+          <label>
+            <input type="checkbox" />
+            Item 2
+          </label>
+          <button className="btn btn-danger">Delete</button>
+        </li>
+      </ul>
     </>
   )
 }
